073b69e7c429ccd845672dc515c34e3e898c7bc2
Atakan Arıkan <marqhen@gmail.com>
2018-10-28 22:04:34 +0100

    latest updates

--- calculateAverages.py
+++ calculateAverages.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 
-print(pd.read_csv("menziesResults/averages/before90.csv").describe())
+print(
+    pd.read_csv(
+        "menziesResults/averages/round_robin/random_repos/before90.csv"
+    ).describe()
+)
