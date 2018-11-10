229305a10494d16926332aa2b9996cbd0b401800
atakanarikan <marqhen@gmail.com>
2018-11-10 15:18:36 +0100

    add training notebook

--- train.py
+++ train.py
@@ -1,5 +1,10 @@
-from keras.models import Sequential
-from keras.layers import Dense
-import numpy
-# fix random seed for reproducibility
-numpy.random.seed(7)
+import keras
+import keras.backend as K
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+
+train_df = pd.read_csv("data/riivo/pandasData.csv", sep=",")
+print(train_df.shape)
